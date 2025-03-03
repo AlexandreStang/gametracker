efd20fb1547d39cc7a3c0acd64990cfb5eef5e39
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2025-03-03 10:18:10 -0500

    Implemented Prisma database interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import {prisma} from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+    const games = await prisma.game.findMany()
+
   return (
     <div>
       Hello world
+
+        <ul>
+            {games?.map((game) => (
+                <li>{game.title}, {game.platform}, {game.playtime}</li>
+            ))}
+        </ul>
     </div>
   );
 }
