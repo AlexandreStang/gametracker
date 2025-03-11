0557be8939bac83a294f866d115ae9f46c1cf82c
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2025-03-11 12:58:18 -0400

    Added function to register played games to database

--- src/services/playedGameService.ts
+++ src/services/playedGameService.ts
@@ -1,6 +1,8 @@
 'use server'
 
 import {createOrUpdateGame} from "@/services/gameService";
+import {PlayedGameController} from "@/controllers/playedGameController";
+import {PlatformController} from "@/controllers/platformController";
 
 export interface PlayedGameFormData {
     gameIGDBid: number,
@@ -11,6 +13,15 @@ export interface PlayedGameFormData {
 
 export async function registerPlayedGame(data: PlayedGameFormData) {
 
-    const game = createOrUpdateGame(data.gameIGDBid)
+    const game = await createOrUpdateGame(data.gameIGDBid)
+    const platform = await PlatformController.getByIgdbId(data.platformIGDBid)
+
+    return await PlayedGameController.create({
+        gameId: game.id,
+        platformId: platform ? platform.id : '',
+        playtime: data.playtime,
+        like: data.like,
+        userId: 'cm7xuh4di0000vmxwj7x7am9r',
+    })
 
 }
